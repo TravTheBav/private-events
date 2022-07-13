d3f8e462d464a1f9a5c97f6011228cf92fdae40b
Travis <tzito93@gmail.com>
2022-07-13 16:34:49 -0500

    Events will be linked to their user when created

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,27 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    if user_signed_in?
+      @event = [current_user.created_events.create(event_params)]
+      render :show, status: :created
+    else
+      redirect_to new_user_session_path, alert: "You must be signed in to create an event"
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :location, :date)
+  end
 end
