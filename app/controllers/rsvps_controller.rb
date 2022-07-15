6fb2d505aa5daf270f6249b9eb6c0e9d2fa09376
Travis <tzito93@gmail.com>
2022-07-15 12:09:58 -0500

    Event attendance through the ui in the event#show view

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -1,12 +1,11 @@
 class RsvpsController < ApplicationController
   def create
     if user_signed_in?
-      @event = Event.find(params[:id])
+      @event = Event.find(params[:event_id])
       current_user.attended_events << @event
-      @event.attendees << current_user
       redirect_to root_path, notice: "rsvp successful!"
     else
-      redirect_to new_user_session_path, alert: "You must be signed in to create an event"
+      redirect_to new_user_session_path, alert: "You must be signed in to attend this event"
     end
   end
 end
